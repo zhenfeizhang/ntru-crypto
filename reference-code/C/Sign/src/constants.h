1b1b9bc785925f048cbbefc3381ace8ba9abf122
John M. Schanck <jschanck@securityinnovation.com>
2013-09-25 18:16:28 -0400

    CPASSREF: Updated makefiles for new directory structure

--- reference-code/C/Sign/src/constants.h
+++ reference-code/C/Sign/src/constants.h
@@ -33,10 +33,10 @@
 #define PASS_k 4095
 #define PASS_b 19
 #define PASS_t 200
-#define PASS_RADER_POLY "data/433_rader.dat"
-#define PASS_PERMUTATION "data/433_perm.dat"
-#define PASS_EVAL_POINTS "data/433_points.dat"
-#define PASS_WISDOM "data/433_wisdom.dat"
+#define PASS_RADER_POLY "../data/433_rader.dat"
+#define PASS_PERMUTATION "../data/433_perm.dat"
+#define PASS_EVAL_POINTS "../data/433_points.dat"
+#define PASS_WISDOM "./data/433_wisdom.dat"
 #endif
 
 #if PASS_N ==  577
@@ -45,10 +45,10 @@
 #define PASS_k 16383
 #define PASS_b 24
 #define PASS_t 280
-#define PASS_RADER_POLY "data/577_rader.dat"
-#define PASS_PERMUTATION "data/577_perm.dat"
-#define PASS_EVAL_POINTS "data/577_points.dat"
-#define PASS_WISDOM "data/577_wisdom.dat"
+#define PASS_RADER_POLY "../data/577_rader.dat"
+#define PASS_PERMUTATION "../data/577_perm.dat"
+#define PASS_EVAL_POINTS "../data/577_points.dat"
+#define PASS_WISDOM "./data/577_wisdom.dat"
 #endif
 
 
@@ -58,10 +58,10 @@
 #define PASS_k 32767
 #define PASS_b 29
 #define PASS_t 386
-#define PASS_RADER_POLY "data/769_rader.dat"
-#define PASS_PERMUTATION "data/769_perm.dat"
-#define PASS_EVAL_POINTS "data/769_points.dat"
-#define PASS_WISDOM "data/769_wisdom.dat"
+#define PASS_RADER_POLY "../data/769_rader.dat"
+#define PASS_PERMUTATION "../data/769_perm.dat"
+#define PASS_EVAL_POINTS "../data/769_points.dat"
+#define PASS_WISDOM "./data/769_wisdom.dat"
 #endif
 
 #if PASS_N == 1153
@@ -70,10 +70,10 @@
 #define PASS_k 32767
 #define PASS_b 36
 #define PASS_t 600
-#define PASS_RADER_POLY "data/1153_rader.dat"
-#define PASS_PERMUTATION "data/1153_perm.dat"
-#define PASS_EVAL_POINTS "data/1153_points.dat"
-#define PASS_WISDOM "data/1153_wisdom.dat"
+#define PASS_RADER_POLY "../data/1153_rader.dat"
+#define PASS_PERMUTATION "../data/1153_perm.dat"
+#define PASS_EVAL_POINTS "../data/1153_points.dat"
+#define PASS_WISDOM "./data/1153_wisdom.dat"
 #endif
 
 
